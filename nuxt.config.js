17c45dbe8871e3fea01d35e3e58fd773da04fc93
Dmitriy <blackbim123@gmail.com>
2020-05-21 03:03:41 +1000

    1

---
+++ nuxt.config.js
@@ -0,0 +1,56 @@
+
+module.exports = {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, minimal-ui' },
+	  { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+    ],
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#FB1C1C' },
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+  // some nuxt config...
+  css: [
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+	 'fullpage-nuxt-module',
+	 'nuxt-fullpage.js',
+  ],
+	axios: {
+		baseURL: process.env.BASE_URL || 'http://localhost:3000'
+		//baseURL: ' https://boulder-new.ru'
+	},
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,56 @@
+<template>
+	<div>
+
+		<full-page ref="fullpage" :options="options">
+			<div class="section main-section">
+				1
+				<button @click="testAction = !testAction">Test Action Button</button>
+				<div v-if="testAction">Hidden block</div>
+			</div>
+			<div class="section player-section">
+				2
+			</div>
+			<div class="section characteristics-section">
+				3
+			</div>
+			<div class="section fp-auto-height">
+				<div style="height: 300px;display:flex; justify-content: center;align-items: center;font-size:40px;"class="inner-footer">
+					Footer
+				</div>
+			</div>
+		</full-page>
+	</div>
+</template>
+
+<script>
+
+
+	export default {
+		name: "ModelPage",
+		data() {
+			return {
+				testAction:false,
+				options: {
+					licenseKey: 'C0E668ED-D0884799-98266D1B-35FA0817',
+					navigation: true,
+					verticalCentered: false,
+					anchors: ['main', 'player', 'info',  'footer'],
+					sectionsColor: ['#41b883', '#ff5f45', '#0798ec'],
+					responsiveHeight: 0,
+					navigationTooltips: ['anchor1', 'anchor2', 'anchor3'],
+				},
+
+
+			}
+		},
+	}
+</script>
+
+<style scoped>
+	.section{
+		display:flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+	}
+</style>
